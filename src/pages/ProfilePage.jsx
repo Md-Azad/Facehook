9ee08452cdc9aa74fba253bc5caa6a17543826a7
Md-Azad <sokalazad@gmail.com>
2024-03-05 11:10:07 +0100

    created reducer to fetched profile data

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,44 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
 import useAxios from "../hooks/useAxios";
+import { useProfile } from "../hooks/useProfile";
+import { actions } from "../action";
 
 const ProfilePage = () => {
     const {auth} = useAuth()
-    const [user, setUser] = useState(null)
-    const [post, setPost] = useState([])
-
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-
+    
+  const {state, dispatch} = useProfile();
     const {api} = useAxios();
 
     useEffect(()=>{
-        setLoading(true)
+        
+      dispatch({type: actions.profile.DATA_FETCHING})
         const fetchProfile = async()=>{
             try{
              const response =    await api.get(`${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user?.id}`)
-                setUser(response?.data?.user);
-                setPost(response?.data?.posts)
+                if(response.status===200){
+                  dispatch({type:actions.profile.DATA_FETCHED, data: response.data})
+                }
             } catch(error){
                 console.log(error)
-                setError(error)
-            }finally{
-                setLoading(false)
-            }
+                dispatch({type: actions.profile.DATA_FETCH_ERROR,error:error.message})
+              }
         }
         fetchProfile();
-    },[api,auth.user.id])
+    },[dispatch,api, auth?.user?.id])
 
-    if(loading){
+    if(state?.loading){
         return <div>Your Profile is loading</div>
     }
   return (
     <div>
-      <p>welcome {user?.firstName}</p>
-      <p>you have {post.length} posts.</p>
-      {
-        error && <p>{error}</p>
-      }
+      <p>welcome {state?.user?.firstName} {state?.user?.lastName}</p>
+      <p>you have {state.posts.length} posts.</p>
+      
     </div>
   );
 };
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,14 +1,16 @@
 import { useAuth } from "../hooks/useAuth";
 import { Navigate,Outlet } from "react-router-dom";
 import Header from "../components/common/Header";
+import ProfileProvider from "../providers/ProfileProvider.jsx";
 
 const PrivateRoutes = () => {
   const { auth } = useAuth();
   
   return (
     <>
-      {auth.user ? (
+      {auth.authToken ? (
         <>
+        <ProfileProvider>
         <Header />
         <main className="mx-auto max-w-[1020px] py-8">
           <div className="container">
@@ -16,6 +18,7 @@ const PrivateRoutes = () => {
             <Outlet></Outlet>
           </div>
         </main>
+        </ProfileProvider>
         </>
       ) : (
         <Navigate to="/login" replace={true} />
